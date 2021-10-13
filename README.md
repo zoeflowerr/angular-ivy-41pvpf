752daf19ddbcad50dc05c18248eeda7cd1e6908e
zoeflowerr <zoe.flower@diegesis.co.uk>
2021-10-13 14:22:35 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-41pvpf
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-41pvpf)
---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component, VERSION } from '@angular/core';
+import { Observable } from 'rxjs'
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Angular ' + VERSION.major;
+}
